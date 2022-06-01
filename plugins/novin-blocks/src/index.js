b95e0c57858b76b2c655ebcb7b15ee197829d72e
Ryan Welcher <me@ryanwelcher.com>
2022-06-01 15:15:52 +0100

    Remove index.js from the src directory. The scripts pacakge can autodetect blocks inside of src.

--- plugins/novin-blocks/src/index.js
+++
@@ -1,7 +0,0 @@
-import "../includes/block-editor/blocks/block-one";
-import "../includes/block-editor/blocks/block-two";
-import "../includes/block-editor/blocks/block-three";
-import "../includes/block-editor/blocks/block-four";
-import "../includes/block-editor/blocks/block-five";
-import "../includes/block-editor/blocks/block-six";
-import "../includes/block-editor/blocks/block-seven";
